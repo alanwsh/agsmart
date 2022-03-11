8a2b85f6217e11d80c8f2630a76af8c9ee3b6f86
Wong <alan_wsh@hotmail.com>
2022-03-11 10:06:31 +0800

    edit db config

--- config/database.php
+++ config/database.php
@@ -2,12 +2,6 @@
 
 use Illuminate\Support\Str;
 
-$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$host = $url["host"] ?? null;
-$username = $url["user"] ?? null;
-$password = $url["pass"] ?? null;
-$database = substr($url["path"], 1);
-
 return [
 
     /*
@@ -51,12 +45,13 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => 'us-cdbr-east-05.cleardb.net',
-            'port' => '3306',
-            'database' => 'heroku_dcfb965e7cd9251',
-            'username' => 'b8e6c4b1dde9bf',
-            'password' => 'a51a9856',
-            'unix_socket' => '',
+            'url' => env('DATABASE_URL'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
